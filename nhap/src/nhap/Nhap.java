bd9a37bbf819995c240360ef28c4be0f75e1d075
Duchvph17480 <duchvph17480@fpt.edu.vn>
2021-11-19 22:35:00 +0700

    Update Nhap.java

--- nhap/src/nhap/Nhap.java
+++ nhap/src/nhap/Nhap.java
@@ -16,5 +16,9 @@ public class Nhap {
      */
     public static void main(String[] args) {
         System.out.println("");
+        System.out.println("");
+        for (int i = 0; i < 10; i++) {
+            
+        }
     }
 }
